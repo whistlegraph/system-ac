934946fd388c02ee203c63ca9d3433e03d117ad6
Jeffrey Scudder <whistlegraph@Jeffreys-MacBook-Air.local>
2021-08-14 13:13:33 -0400

    setup offline development environment

--- public/boot.js
+++ public/boot.js
@@ -1,9 +1,12 @@
 import { boot } from "./computer/bios.js";
-const host = "disk.whistlegraph.com"; 
+// const host = "disk.whistlegraph.com";
+const host = "127.0.0.1:8081";
 
 // For now there is no interactive boot menu, so we load a disk directly.
-boot("metronome-test", host);
-// boot("doodle", host);
+//boot("metronome-test", host);
+
+boot("doodle", host);
+
 // boot("starfield", host);
 // load("blank", host);
 // load("worker-disk", host);
